3a81ab47d8f568228261993834a1cf66fbb1894d
ingalb <ingalb@users.noreply.github.com>
2015-04-20 00:00:03 +0200

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -5,6 +5,6 @@
 
 //app config - replace values with ids generated in PushApps admin console and GCM console
 app.constant('config', {
-	"googleProjectNumber": "",//optional - only if you are supporting android. If not leave empty.
-	"pushAppsAppToken": "" 
+	"googleProjectNumber": "455582282730",//optional - only if you are supporting android. If not leave empty.
+	"pushAppsAppToken": "9128f99a-4783-4c6e-803d-a77f13d332ca" 
 })
